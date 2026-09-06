731391b34b61dc251e01859737fc58dd1caa3de9
ishikaj0408 <ishikaj42008@gmail.com>
2026-09-06 15:41:31 +0530

    Update login.py

--- gui/login.py
+++ gui/login.py
@@ -62,7 +62,7 @@ def connect_database():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Ishika@123",
+            password="",
             database="movie_recommendation"
         )
 
